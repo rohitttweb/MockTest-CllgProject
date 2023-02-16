8baf6836886f612b6e2018b2a432e46c1685a388
rohitttweb <rohitmehra3216@gmail.com>
2023-02-16 08:15:12 +0530

    16-02-2023 08:15

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
 const Questionss = {
 
     "questions": [
